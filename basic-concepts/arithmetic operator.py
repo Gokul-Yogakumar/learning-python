c9df7ee187934f97a01c3c00a79a203ea97ecd66
Gokul-Yogakumar <146834020+Gokul-Yogakumar@users.noreply.github.com>
2023-10-30 09:11:36 +0530

    Add files via upload

---
+++ basic-concepts/arithmetic operator.py
@@ -0,0 +1,23 @@
+a=4
+b=6
+
+#Addition
+print('a + b =',a+b)
+
+#Subtraction
+print('a - b =',a-b)
+
+#Multiplication
+print('a * b =',a*b)
+
+#Division
+print('a / b =',a/b)
+
+#Modulo division, prints the remainder
+print('a % b=',a%b)
+
+#Floor division
+print('a // b =',a//b)
+
+#Exponent a to the power b
+print('a ** b =',a**b)
